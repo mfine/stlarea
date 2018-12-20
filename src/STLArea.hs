5ca81b8fcbae84593603947741f481559fc5f8c4
Mark Fine <mark.fine@gmail.com>
2018-12-20 01:14:23 -0500

    Absolute values.

--- src/STLArea.hs
+++ src/STLArea.hs
@@ -6,7 +6,7 @@ distance :: Vector -> Vector -> Float
 distance (a, b, c) (x, y, z) = sqrt $ (a - x) ** 2 + (b - y) ** 2 + (c - z) ** 2
 
 area :: Triangle -> Float
-area triangle = sqrt $ s * (s - x) * (s - y) * (s - z)
+area triangle = sqrt $ s * abs (s - x) * abs (s - y) * abs (s - z)
   where
     (a, b, c) = vertices triangle
     x         = distance a b
